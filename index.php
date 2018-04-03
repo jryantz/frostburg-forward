e75e4ef20a54fbe8d33f3b143a11122cfb417ea3
jatripp0 <jatripp0@users.noreply.github.com>
2018-04-03 19:46:57 -0400

    Updated PHP array creation, begun coding javascript

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if($con->connect_error) {
 echo 'Connection Successful'.
 
 $questions = array();
+$question_flow = array();
 
 $sql = "SELECT * FROM questions";
 $result = $con->query($sql);
@@ -18,55 +19,40 @@ $result = $con->query($sql);
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $questions[$row['id']] = $row['question'];
-        // Decode json...
+        $question_flow[$row['id']] = $row['flow'];
     }
 } else {
     echo 'No Results';
 }
 
-$answers = array(array());
+$answers = array();
 
 $sql = "SELECT * FROM answers";
 $result = $con->query($sql);
 
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        array_push($answers[$row['question_id']], $row['answer']);
-        //$answers[$row['question_id']] = $row['answer'];
+        $answers[$row['question_id']][] = $row['answer'];
     }
 } else {
     echo 'No Results';
 }
 
-$question_flow = array();
-
-$sql = "SELECT * FROM question_flow";
+$resources = array();
+$sql = "SELECT * FROM resources";
 $result = $con->query($sql);
+
+if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $answers[$row['answer_id']] = array($row['link'], $row['text']);
+    }
+} else {
+    echo 'No Results';
+}
+
 ?>
 
 <html>
     <head></head>
     <body></body>
 </html>
-
-// Option 1 - with question flow table
-question1 -> question2 = {
-    "answer": {1,2}
-}
-
-question1 -> question3 = {
-    "answer": {3}
-}
-
-question1 -> question4 = {
-    "answer": {4,5}
-}
-
-// Option 2 - housed in question table
-{
-    "answer-1": 2,
-    "answer-2": 2,
-    "answer-3": 3,
-    "answer-4": 4,
-    "answer-5": 4
-}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+<script>
+  var questions = <?php echo json_encode($questions); ?>;
+  var answers = <?php echo json_encode($answers); ?>;
+  var question_flow = <?php echo json_encode($question_flow); ?>;
+  var resources = <?php echo json_encode($resources); ?>;
+</script>
