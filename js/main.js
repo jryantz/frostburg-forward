146417f082cb99c322c5a4bb9d5072cc708ed15d
jatripp0 <jatripp0@users.noreply.github.com>
2018-04-22 15:00:00 -0400

    Building report page, generating user session information, added image to main page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-<script>
-  var questions = <?php echo json_encode($questions); ?>;
-  var answers = <?php echo json_encode($answers); ?>;
-  var question_flow = <?php echo json_encode($question_flow); ?>;
-  var resources = <?php echo json_encode($resources); ?>;
-</script>
+var session_report;
+
+function setSessionReport(jsonObj){
+  session_report = jsonObj;
+}
+
+function getSessionReport(){
+  return session_report;
+}
