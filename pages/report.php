631008b10c049fef282a0f128afe3e09680c7d4c
kmleedy <kmbates14@gmail.com>
2018-05-15 11:04:17 -0400

    seals on report

--- pages/report.php
+++ pages/report.php
@@ -368,25 +368,23 @@ if($con->connect_error) { die('Connection Failed: ' . $con->connect_error); }
           return regex.test(address);
         }
         </script>
-        <footer><div></div></footer>
-        <!-- <footer>
+        <footer>
             <div>
                 <div class="seals">
                   <a href= "https://alleganyworks.org/">
-                      <img src="assets/County Econ Logo-CIRCLE.jpg">
+                      <img src="pages/assets/County Econ Logo-CIRCLE.jpg">
                   </a>
                   <a href= "http://www.marylandsbdc.org/locations/western-region">
-                      <img src="assets/CitySeal.png">
+                      <img src="pages/assets/CitySeal.png">
                   </a>
                   <a href= "https://www.choosecumberland.org/">
-                      <img src="assets/CEDC_full-color.png">
+                      <img src="pages/assets/CEDC_full-color.png">
                   </a>
                   <a href = "https://www.garrettcounty.org/">
-                      <img src="assets/Garrett-County-Seal-2.png">
+                      <img src="pages/assets/Garrett-County-Seal-2.png">
                   </a>
                 </div>
-                <p>Photo Credit: Gerald Snelson</p>
             </div>
-        </footer> -->
+        </footer>
     </body>
 </html>
