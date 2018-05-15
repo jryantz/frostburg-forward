9365fe319ea2e315413a3fdb7e841364db9ac428
kmleedy <kmbates14@gmail.com>
2018-05-15 11:06:25 -0400

    remove seals

--- pages/report.php
+++ pages/report.php
@@ -368,23 +368,5 @@ if($con->connect_error) { die('Connection Failed: ' . $con->connect_error); }
           return regex.test(address);
         }
         </script>
-        <footer>
-            <div>
-                <div class="seals">
-                  <a href= "https://alleganyworks.org/">
-                      <img src="pages/assets/County Econ Logo-CIRCLE.jpg">
-                  </a>
-                  <a href= "http://www.marylandsbdc.org/locations/western-region">
-                      <img src="pages/assets/CitySeal.png">
-                  </a>
-                  <a href= "https://www.choosecumberland.org/">
-                      <img src="pages/assets/CEDC_full-color.png">
-                  </a>
-                  <a href = "https://www.garrettcounty.org/">
-                      <img src="pages/assets/Garrett-County-Seal-2.png">
-                  </a>
-                </div>
-            </div>
-        </footer>
     </body>
 </html>
